d72d396c73a7fc62a913b4c0971275f2266a4858
Siwakorn Banluesapy <siwakorn167@gmail.com>
2023-08-22 14:50:33 +0700

    new change

--- src/section/BlankPage.jsx
+++ src/section/BlankPage.jsx
@@ -5,8 +5,8 @@ export default class BlankPage extends Component {
     return (
       <div>
         <div align="center" style={{ padding: "150px" }}>
-          <h1>DMS</h1>
-          <h3>ระบบบริหารโรงเรียนสอนขับรถ</h3>
+          <h1>DTMS</h1>
+          <h3>ระบบบริหารการจัดการศูนย์สอนขับรถขั้นพื้นฐาน</h3>
         </div>
       </div>
     );
